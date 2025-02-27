f832ec10e0de9c2a55e8c45b9ba57b98d53b0367
timmy <timhansher@gmail.com>
2025-02-27 10:18:25 -0500

    remove chat values

--- lua/oh-lucy/theme.lua
+++ lua/oh-lucy/theme.lua
@@ -618,9 +618,9 @@ M.plugins = {
     RenderMarkdownQuote = { fg = colors.fg, bg = colors.context },
     RenderMarkdownUnchecked = { fg = colors.pink },
     RenderMarkdownChecked = { fg = colors.yellow },
-    ChatSystem = { fg = colors.fg, bg = colors.context },
-    ChatUser = { fg = colors.fg, bg = colors.red_key_w },
-    ChatAssistant  = { fg = colors.fg, bg = colors.yellow }
+    -- ChatSystem = { fg = colors.fg, bg = colors.context },
+    -- ChatUser = { fg = colors.fg, bg = colors.red_key_w },
+    -- ChatAssistant  = { fg = colors.fg, bg = colors.yellow }
 }
 
 return M
