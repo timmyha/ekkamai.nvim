33bdfc59873872b8ec460795c6109d8b6a4f02fd
timmy <timhansher@gmail.com>
2025-03-02 02:57:44 -0500

    test

--- lua/oh-lucy/colors.lua
+++ lua/oh-lucy/colors.lua
@@ -1,11 +1,57 @@
+-- return {
+--   fg               = "#ced6d1",
+--   bg               = "#171314",
+--   none             = "#171314",
+--   --14161D
+--   dark             = '#141d17',
+--   comment          = "#4C544F",
+--   popup_back       = '#4C544F',
+--   cursor_fg        = '#ced6d1',
+--   context          = '#516158',
+--   cursor_bg        = '#aeafad',
+--   accent           = '#BBBBBB',
+--   diff_add         = '#8CD881',
+--   diff_change      = '#6CAEC0',
+--   cl_bg            = "#709176",
+--   diff_text        = '#568BB4',
+--   line_fg          = "#4C544F",
+--   line_bg          = "#171314",
+--   gutter_bg        = "#171314",
+--   non_text         = "#60786b",
+--   selection_bg     = "#5e7e6d",
+--   selection_fg     = "#496351",
+--   vsplit_fg        = "#cccccc",
+--   vsplit_bg        = "#141d17",
+--   visual_select_bg = "#963c3c",
+--
+--   red_key_w  = "#96596d",
+--   red_err    = "#963c3c",
+--   green_func = '#578f77',
+--   green      = "#578f77",
+--   blue_type  = '#577d91',
+--   black1     = "#272932",
+--   black      = "#0a0d0b",
+--   white1     = "#D7D7D7",
+--   white      = "#E9E9E9",
+--   gray_punc  = "#7C7E8C",
+--   gray2      = "#6E7380",
+--   gray1      = '#343842',
+--   gray       = "#21252D",
+--   orange     = "#b86c75",
+--   boolean    = "#66578a",
+--   orange_wr  = "#a37a5b",
+--   pink       = "#66578a",
+--   yellow     = "#a3985b",
+-- }
+
 return {
-  fg               = "#ced6d1",
-  bg               = "#171314",
-  none             = "#171314",
+  fg               = "#ffffff",
+  bg               = "#161616",
+  none             = "#161616",
   --14161D
-  dark             = '#141d17',
-  comment          = "#4C544F",
-  popup_back       = '#4C544F',
+  dark             = '#262626',
+  comment          = "#525252",
+  popup_back       = '#525252',
   cursor_fg        = '#ced6d1',
   context          = '#516158',
   cursor_bg        = '#aeafad',
@@ -24,23 +70,38 @@ return {
   vsplit_bg        = "#141d17",
   visual_select_bg = "#963c3c",
 
-  red_key_w  = "#96596d",
+  red_key_w  = "#ee5396",
   red_err    = "#963c3c",
-  green_func = '#578f77',
-  green      = "#578f77",
-  blue_type  = '#577d91',
-  black1     = "#272932",
-  black      = "#0a0d0b",
-  white1     = "#D7D7D7",
-  white      = "#E9E9E9",
-  gray_punc  = "#7C7E8C",
-  gray2      = "#6E7380",
+  green_func = '#42be65',
+  green      = "#3ddbd9",
+  blue_type  = '#33b1ff',
+  black1     = "#262626",
+  black      = "#393939",
+  white1     = "#f2f4f8",
+  white      = "#dde1e6",
+  gray_punc  = "#dde1e6",
+  gray2      = "#525252",
   gray1      = '#343842',
   gray       = "#21252D",
-  orange     = "#b86c75",
-  boolean    = "#66578a",
+  orange     = "#ff7eb6",
+  boolean    = "#be95ff",
   orange_wr  = "#a37a5b",
-  pink       = "#66578a",
-  yellow     = "#a3985b",
-
+  pink       = "#be95ff",
+  yellow     = "#82cfff",
 }
+        -- base00 = "#161616";
+        -- base01 = "#262626";
+        -- base02 = "#393939";
+        -- base03 = "#525252";
+        -- base04 = "#dde1e6";
+        -- base05 = "#f2f4f8";
+        -- base06 = "#ffffff";
+        -- base07 = "#08bdba";
+        -- base08 = "#3ddbd9";
+        -- base09 = "#78a9ff";
+        -- base0A = "#ee5396";
+        -- base0B = "#33b1ff";
+        -- base0C = "#ff7eb6";
+        -- base0D = "#42be65";
+        -- base0E = "#be95ff";
+        -- base0F = "#82cfff";
